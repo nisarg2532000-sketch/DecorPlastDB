a2f1009ff5f43d955e5553c99f48e37661e721cd
nisarg2532000-sketch <nisarg2532000@gmail.com>
2026-04-20 23:25:48 +0530

    Rename dbo.USP_GetUserByIds.StoredProcedure.sql to StoredProcedures /dbo.USP_GetUserByIds.sql

---
+++ StoredProcedures /dbo.USP_GetUserByIds.sql
@@ -0,0 +1,26 @@
+﻿USE [DecorPlastDB]
+GO
+/****** Object:  StoredProcedure [dbo].[USP_GetUserByIds]    Script Date: 03-03-2026 10.48.26 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[USP_GetUserByIds]
+    @Id INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT 
+        Id,
+        Name,
+        MobileNo
+    FROM tblUser
+    WHERE Id = @Id;
+END
+GO
